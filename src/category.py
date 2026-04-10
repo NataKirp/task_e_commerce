e85a8f576304ebb4e1d990438042fd70b89d93da
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-10 17:08:04 +0300

    Исправления после проверки линтерами

--- src/category.py
+++ src/category.py
@@ -24,7 +24,7 @@ class Category:
         total_category_products = 0
         for product in self.__products:
             total_category_products += product.quantity
-        return f'{self.name}, количество продуктов: {total_category_products} шт.'
+        return f"{self.name}, количество продуктов: {total_category_products} шт."
 
     def add_product(self, product: Product):
         """Метод для добавления продукта в категорию."""
@@ -35,7 +35,7 @@ class Category:
     @property
     def products(self):
         """Геттер для вывода списка товаров"""
-        product_str = ''
+        product_str = ""
         for product in self.__products:
-            product_str += f'{str(product)}\n'
+            product_str += f"{str(product)}\n"
         return product_str
--- src/product.py
+++ src/product.py
@@ -6,7 +6,7 @@ class Product:
     price: float
     quantity: int
 
-    products_dict = []
+    products_dict: list = []
 
     def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса."""
@@ -17,18 +17,18 @@ class Product:
         Product.products_dict.append(self)
 
     def __str__(self):
-        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        return self.quantity * self.price + other.quantity * other.price
+        return self.quantity * self.__price + other.quantity * other.__price
 
     @classmethod
     def new_product(cls, products_dict):
         """Метод для создания нового объекта класса"""
-        name = products_dict.get('name')
-        description = products_dict.get('description')
-        price = products_dict.get('price')
-        quantity = products_dict.get('quantity')
+        name = products_dict.get("name")
+        description = products_dict.get("description")
+        price = products_dict.get("price")
+        quantity = products_dict.get("quantity")
         return cls(name, description, price, quantity)
 
     @property
@@ -40,6 +40,6 @@ class Product:
     def price(self, new_price):
         """Сеттер для проверки на положительное значение новой цены."""
         if new_price <= 0:
-            print('Цена не должна быть нулевая или отрицательная')
+            print("Цена не должна быть нулевая или отрицательная")
             return
         self.__price = new_price
