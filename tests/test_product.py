5f4ac3e51cc217bbe80f2740bfe2d8c07c6c3c5d
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-11 19:38:13 +0300

    Добавлены тесты и фикстуры на новую функциональность. Обновлен отчет html

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.product import Product
 
 
@@ -29,3 +31,47 @@ def test_product_str(product):
 
 def test_product_add(product_1, product_2):
     assert product_1 + product_2 == 2580000.0
+
+
+def test_smartphone_init(smartphone_1):
+    assert smartphone_1.name == 'Samsung Galaxy S23 Ultra'
+    assert smartphone_1.description == '256GB, Серый цвет, 200MP камера'
+    assert smartphone_1.price == 180000.0
+    assert smartphone_1.quantity == 5
+    assert smartphone_1.efficiency == 95.5
+    assert smartphone_1.model == 'S23 Ultra'
+    assert smartphone_1.memory == 256
+    assert smartphone_1.color == 'Серый'
+
+
+def test_smartphone_add(smartphone_1, smartphone_2):
+    assert smartphone_1 + smartphone_2 == 2580000
+
+
+def test_smartphone_add_not_product(smartphone_1):
+    with pytest.raises(TypeError):
+        result = smartphone_1 + 'not a product'
+
+
+def test_lawn_grass_init(grass_1):
+    assert grass_1.name == 'Газонная трава'
+    assert grass_1.description == 'Элитная трава для газона'
+    assert grass_1.price == 500.0
+    assert grass_1.quantity == 20
+    assert grass_1.country == 'Россия'
+    assert grass_1.germination_period == '7 дней'
+    assert grass_1.color == 'Зеленый'
+
+
+def test_lawn_grass_add(grass_1, grass_2):
+    assert grass_1 + grass_2 == 16750
+
+
+def test_lawn_grass_add_not_product(grass_1):
+    with pytest.raises(TypeError):
+        result = grass_1 + 'not a product'
+
+
+def test_diff_category_add_error(smartphone_1, grass_1):
+    with pytest.raises(TypeError):
+        result = smartphone_1 + grass_1
