26253b73f14fefeb341d07c96cc8b6035feeddf1
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-11 17:51:55 +0300

    Добавлены классы-наследники ль Product: Smartphone и LawnGrass

--- src/product.py
+++ src/product.py
@@ -43,3 +43,20 @@ class Product:
             print("Цена не должна быть нулевая или отрицательная")
             return
         self.__price = new_price
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
