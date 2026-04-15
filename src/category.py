a3472655952d57210100828de1dd543c77fe8ddc
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-16 00:01:21 +0300

    Обновлены category.py и product.py после проверки линтерами

--- src/category.py
+++ src/category.py
@@ -42,10 +42,11 @@ class Category:
             product_str += f"{str(product)}\n"
         return product_str
 
-
     def middle_price(self):
         """Метод для подсчета среднего ценника товаров"""
         try:
-            return sum([product.price for product in self.__products]) / len(self.__products)
+            return sum([product.price for product in self.__products]) / len(
+                self.__products
+            )
         except ZeroDivisionError:
             return 0
--- src/product.py
+++ src/product.py
@@ -20,7 +20,7 @@ class Product(BaseProduct, PrintMixin):
         if quantity > 0:
             self.quantity = quantity
         else:
-            raise ValueError('Товар с нулевым количеством не может быть добавлен.')
+            raise ValueError("Товар с нулевым количеством не может быть добавлен.")
         Product.products_dict.append(self)
         super().__init__()
 
